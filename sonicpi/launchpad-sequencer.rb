a15870cce131c82bb286e4a351517ba4e9eab48e
Josh Nichols <technicalpickles@github.com>
2020-09-24 19:51:28 -0400

    Update launchpad-sequencer.rb

--- sonicpi/launchpad-sequencer.rb
+++ sonicpi/launchpad-sequencer.rb
@@ -1,6 +1,8 @@
-# Blip Rhythm
 
-# Coded by Sam Aaron
+require "/Users/technicalpickles/hi.rb"
+
+
+
 
 sps = "/Users/technicalpickles/Dropbox/Music Making/Sample Packs"
 br = "#{sps}/The Synth Sounds of Blade Runner - Reverb Sample Pack"
@@ -10,7 +12,7 @@ brfx = "#{br}/FX"
 
 
 load_samples [:drum_heavy_kick, :elec_plip, :elec_blip]
-use_bpm 100
+use_bpm 160
 
 static_color = 1
 flashing_color = 2
@@ -20,7 +22,7 @@ launchpad_out = "launchpad_x_lpx_midi_in"
 
 midi_note_off 81, 56, port: launchpad_out
 
-offsets =[
+offsets = [
   81,
   71,
   61,
@@ -34,12 +36,10 @@ offsets =[
 midi_out = "iac_driver_sonic_pi"
 
 
-
-
 teal = 37
 pink = 56
 
-note_length = 0.25
+beat_length = 1
 instruments = (0..7).to_a
 state = instruments.map do |instrument|
   {}
@@ -70,8 +70,12 @@ define :toggle_step do |instrument, note|
 end
 
 
-offsets.each do |offset|
-  instruments.each do |instrument|
+# reset everything
+offsets.each_with_index do |offset, y|
+  instruments.each_with_index do |instrument, x|
+    note = offset + instrument
+    puts "#{y},#{x}: note #{note}, off"
+    
     step_off(instrument, offset + instrument)
     ##| if dice(2) == 2
     ##|   step_on(instrument, offset+instrument)
@@ -79,71 +83,75 @@ offsets.each do |offset|
     ##|   step_off(instrument, offset+instrument)
     ##| end
   end
-  
 end
 
-define :pink_on do |note|
-  
-  midi_note_on note, pink, port: launchpad_out, channel: static_color
-  puts "pink on #{note}"
-end
-
-define :pink_off do |note|
-  midi_note_off note, pink, port: launchpad_out, channel: static_color
-end
-
-
-live_loop :clock do
-  use_real_time
-  
-  offsets.each do |offset|
-    
-    instruments.each do |instrument|
-      note = offset + instrument
-      pink_on(note)
+in_thread do
+  loop do
+    offsets.each_with_index do |offset, y|
+      puts "row #{y}"
       
-      if play_step?(instrument, note)
-        midi_note_on note, port: midi_out, channel: 1
-      end
-      
+      cue "tick", offset: offset, y: y
+      sleep beat_length
+      cue "tock", offset: offset, y: y
     end
-    
-    sleep note_length
-    
-    instruments.each do |instrument|
-      note = offset + instrument
-      pink_off(note)
-      
-      if play_step?(instrument, note)
-        # end the last note
-        midi_note_off note, port: midi_out, channel: 1
-        # set it back to the toggled color
-        midi_note_on note, teal, port: launchpad_out, channel: static_color
-      end
-    end
-    
-    sleep note_length
   end
+end
+
+in_thread do
+  loop do
+    values = sync "tick"
+    puts "tick"
+    
+    offset = values[:offset]
+    x = values[:y]
+    
+    instruments.each_with_index do |instrument, x|
+      note = offset + instrument
+      
+      midi_note_on note, pink, port: launchpad_out, channel: static_color
+      
+      if play_step?(instrument, note)
+        cue "instrument_#{instrument}"
+      end
+    end
+  end
+  
+end
+
+in_thread do
+  loop do
+    
+    values = sync "tock"
+    puts "tock"
+    
+    offset = values[:offset]
+    y = values[:y]
+    instruments.each_with_index do |instrument, x|
+      note = offset + instrument
+      
+      # set it back to the toggled color if it was played
+      if play_step?(instrument, note)
+        puts "#{y},#{x}: note #{note}, blue"
+        midi_note_on note, teal, port: launchpad_out, channel: static_color
+      else
+        puts "#{y},#{x}: note #{note}, off"
+        midi_note_off note, port: launchpad_out, channel: static_color
+      end
+    end
+  end
+  
 end
 
 live_loop :toggles do
   use_real_time
   note, velocity = sync "/midi:launchpad_x_lpx_midi_out:4:1/note_on"
-  instrument = note_to_instrument(note)
-  puts "instrument #{instrument}"
-  toggle_step(instrument, note)
   
-  # sleep a little to prevent key up from triggering it again
-  sleep note_length
-end
-
-
-live_loop :instrument do
-  use_real_time
-  note, velocity = sync "/midi:#{midi_out}:1:1/note_on"
-  instrument = note_to_instrument(note)
-  
-  sample_step(instrument)
+  # for some reason, release triggers as a note_on with velocity 0?
+  if velocity > 0
+    instrument = note_to_instrument(note)
+    puts "instrument #{instrument}"
+    toggle_step(instrument, note)
+  end
 end
 
 define :note_to_instrument do |note|
@@ -152,24 +160,44 @@ define :note_to_instrument do |note|
   instrument = note % 10 - 1
 end
 
-define :sample_step do |instrument|
-  case instrument
-  when 0
-    sample :drum_heavy_kick, amp: 0.5
-  when 1
-    sample :elec_plip, amp: 0.5
-  when 2
-    sample :elec_blip
-  when 3
-    use_synth :fm
-    play :c2, attack: 0, release: 0.25
-  when 4
-    sample :drum_cymbal_open, attack: 0.1, sustain: 0.1, release: 0.1 # 0.5
-  when 5
-    sample brp, "STOMP 2", sustain: 0, release: 1
-    
-  end
+
+live_loop :instrument_0 do
+  sync "instrument_0"
+  sample :drum_heavy_kick, amp: 0.5
 end
+
+live_loop :instrument_3 do
+  sync "instrument_1"
+  sample :elec_plip, amp: 0.5
+end
+
+live_loop :instrument_2 do
+  sync "instrument_2"
+  sample :elec_blip
+end
+
+live_loop :instrument_3 do
+  sync "/cue/instrument_3"
+  use_synth :fm
+  play :c2, attack: 0, release: 0.25
+end
+
+live_loop :instrument_4 do
+  sync "/cue/instrument_4"
+  sample :drum_cymbal_soft, attack: 0.1, sustain: 0.1, release: 0.1 # 0.5
+end
+
+live_loop :instrument_5 do
+  sync "/cue/instrument_5"
+  sample brp, "STOMP 2", sustain: 0, release: 1
+end
+
+live_loop :instrument_6 do
+  sync "instrument_6"
+  
+  sample brp, "BLADE RUNNER CABASA (DRY)", start: 0, finish: 0.125
+end
+
 
 ##| live_loop :dynamic_kick do
 ##|   use_real_time
